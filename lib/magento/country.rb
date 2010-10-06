7fcec7fe0fb8b91ee7e292b9e4ac327660f516bf
Preston Stuteville <preston.stuteville@gmail.com>
2010-10-06 15:34:59 -0700

    fixed reference to country iso codes

--- lib/magento/country.rb
+++ lib/magento/country.rb
@@ -22,12 +22,12 @@ module Magento
       end
       
       def find_by_iso(iso)
-        list.select{ |c| [c.iso2, c.iso3].include? iso }.first
+        list.select{ |c| [c.iso2_code, c.iso3_code].include? iso }.first
       end
     end
     
     def regions
-      Magento::Region.find_by_country(self.iso2)
+      Magento::Region.find_by_country(self.iso2_code)
     end
   end
 end
