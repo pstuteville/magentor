2135d2ac39b4ab93e0232bab3cb1f68908a6e7e1
eugenebolshakov <eugene.bolshakov@gmail.com>
2011-03-30 09:44:47 +0400

    Added Magento::Inventory.list method

--- lib/magento/inventory.rb
+++ lib/magento/inventory.rb
@@ -1,4 +1,16 @@
 module Magento
   class Inventory < Base
+    class << self
+      def api_path
+        "product_stock"
+      end
+
+      def list(*args)
+        results = commit("list", *args)
+        results.collect do |result|
+          new(result)
+        end
+      end
+    end
   end
 end
