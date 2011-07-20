7d18ab5674f05d30633f9f6746d65aa19fc96278
eugenebolshakov <eugene.bolshakov@gmail.com>
2011-07-20 22:50:35 +0400

    Retry on EOFError, which seems to be very common

--- lib/magento/connection.rb
+++ lib/magento/connection.rb
@@ -9,16 +9,32 @@ module Magento
     
     def connect
       @client = XMLRPC::Client.new(config[:host], config[:path], config[:port])
-      @session = @client.call("login", config[:username], config[:api_key])
+      retry_on_connection_error do
+        @session = @client.call("login", config[:username], config[:api_key])
+      end
     end
     
     def call(method = nil, *args)
       @logger.debug "call: #{method}, #{args.inspect}"
       connect
-      @client.call("call", @session, method, args)
+      retry_on_connection_error do
+        @client.call("call", @session, method, args)
+      end
     rescue XMLRPC::FaultException => e
       @logger.debug "exception: #{e.faultCode} -> #{e.faultString}"
       raise Magento::ApiError, "#{e.faultCode} -> #{e.faultString}"
     end
+
+    private
+
+      def retry_on_connection_error
+        attempts = 0
+        begin
+          yield
+        rescue EOFError
+          attempts += 1
+          retry if attempts < 2
+        end
+      end
   end
 end
